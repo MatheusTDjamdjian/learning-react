0573af361a2a08eed7b30673505d3384c1d5f8d0
Matheus <matheustassodjamdjian@gmail.com>
2024-09-20 17:50:32 -0300

    component

--- content/video-02/first-project/src/App.jsx
+++ content/video-02/first-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HelloWorld from './components/HelloWorld' 
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,12 +11,16 @@ function App() {
 
   const newName = name.toUpperCase()
 
+  function sum(a, b) {
+    return a + b
+  }
+
   return (
     <>
       <h1>My firts project in React</h1>
       <p>Olá, {newName}</p>
       <br/>
-      <p>Soma: { 2 + 2 }</p>
+      <p>Soma: {sum(1, 2)}</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,6 +29,7 @@ function App() {
           Alterando o JSX
         </p>
       </div>
+      <HelloWorld/>
     </>
   )
 }
