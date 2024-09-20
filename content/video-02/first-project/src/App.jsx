26834764c0b1548796af649dacf1b041ed7cdd7c
Matheus <matheustassodjamdjian@gmail.com>
2024-09-20 16:46:44 -0300

    changes

--- content/video-02/first-project/src/App.jsx
+++ content/video-02/first-project/src/App.jsx
@@ -6,28 +6,24 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const name = 'Matheus'
+
+  const newName = name.toUpperCase()
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>My firts project in React</h1>
+      <p>Olá, {newName}</p>
+      <br/>
+      <p>Soma: { 2 + 2 }</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Alterando o JSX
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
