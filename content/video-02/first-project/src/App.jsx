a1f3028316395eed7244321e543b70b386bbce9e
Matheus <matheustassodjamdjian@gmail.com>
2024-09-23 17:22:29 -0300

    events by prop

--- content/video-02/first-project/src/App.jsx
+++ content/video-02/first-project/src/App.jsx
@@ -44,8 +44,7 @@ function App() {
       <HelloWorld/>
       <div>
         <h1>Testando Eventos</h1>
-        <Evento numero="1"/>
-        <Evento numero="2"/>
+        <Evento/>
       </div>
       <Form/>
     </>
--- content/video-02/first-project/src/components/Evento.jsx
+++ content/video-02/first-project/src/components/Evento.jsx
@@ -1,14 +1,22 @@
-function Evento({numero}) {
+import Button from './evento/Button'
+
+function Evento() {
     
     function meuEvento() {
-        alert(`Parabéns! Você ativou o evento. ${numero}`)
-    console.log(`Parabéns! Você ativou o evento. ${numero}`)
+        alert(`Parabéns! Você ativou o primeiro evento.`)
+        console.log(`Parabéns! Você ativou o primeiro evento.`)
+    }
+
+    function segundoEvento() {
+        alert(`Parabéns! Você ativou o segundo evento.`)
+        console.log('Parabéns! Você ativou o segundo evento.')
     }
 
     return (
         <>
         <p>Clique para disparar um evento:</p>
-        <button onClick={meuEvento}>Ativar!</button>
+        <Button event={meuEvento} text="Primeiro Evento"/>
+        <Button event={segundoEvento} text="Segundo Evento"/>
         </>
     )
 }
